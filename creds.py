ed481a75fc5b5910ee38b51ecbb479c9354544a7
Bawwa Official <78470759+OfficialBawwa@users.noreply.github.com>
2021-02-18 22:12:38 +0530

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class Credentials:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
+    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
+    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
+    START_MESSAGE = os.getenv("START_MESSAGE",None) #Not Mandatory
